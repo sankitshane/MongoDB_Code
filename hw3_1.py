1bd9a2a7bb52f351ad75ed47514095ce66994a0d
sankit acharya <sankit.acharya@gmail.com>
2016-10-26 23:51:17 +0530

    hw3.1 complete

--- hw3_1.py
+++ hw3_1.py
@@ -8,28 +8,23 @@ student = db.students
 
 
 def find_and_delete():
-    res = student.find()
-
-    low_score = 0
+    res = student.find({},{'scores':1})
+    low = 0
     for doc in res:
-        for i in doc:
-            if i == 'scores':
-                for j in i:
-                    if j == 'homework':
-                        if(low_score == 0):
-                            low_score = j.score
-                        else:
-                            if(low_score > j.scores):
-                                low_score = j.scores
-        for i in doc:
-            if i == 'scores':
-                for j in i:
-                    if j == 'homework':
-                        if j.score == low_score and j.type =='homework':
-                            doc.scores.remove(i)
-
-    for doc in res:
+        for i in doc["scores"]:
+            if i["type"] == 'homework':
+                if low == 0:
+                    low = i['score']
+                elif low > i['score']:
+                    low = i['score']
+        for i in doc['scores']:
+            if i['score'] == low:
+                doc['scores'].remove(i)
         print doc
+        low = 0
+
+
+        student.update_one({'_id':doc['_id']},{'$set':{'scores':doc['scores']}})
 
 if __name__ == '__main__':
     find_and_delete()
